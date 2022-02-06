70fe1e7e6a6d34bdbff113c4040944bcb61522fd
neeraj15022001 <gneeraj32595@gmail.com>
2022-02-06 16:15:19 +0530

    Basic UI Done with MUI

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+import Body from "../Body/Body"
+
+const App = () => {
+    return (
+        <div>
+            <Body/>
+        </div>
+    );
+};
+
+export default App;
