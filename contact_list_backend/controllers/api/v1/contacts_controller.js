4a178bf93ae5a95f69fdca9af1d05bbb6cc3d73f
neeraj15022001 <gneeraj32595@gmail.com>
2022-02-06 17:21:50 +0530

    update contact route and controller created

--- contact_list_backend/controllers/api/v1/contacts_controller.js
+++ contact_list_backend/controllers/api/v1/contacts_controller.js
@@ -1,14 +1,18 @@
 const Contact = require("../../../models/contact_schema")
 module.exports.new = async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     try {
+        //TODO: check if contact with same email and mobile number
+        //Creating Contact
         let contact = await Contact.create(req.body);
+        //Sending contact to user after creating
         return res.json(200, {
             message: "Successfully created contact",
             data: contact
         })
     } catch (e) {
-        console.log("Error while creating contact", e);
+        // console.log("Error while creating contact", e);
+        //Sending error to user
         return res.json(500, {
             message: 'Error while creating contact',
             error: e
@@ -16,12 +20,16 @@ module.exports.new = async (req, res) => {
     }
 }
 module.exports.delete = async (req, res) => {
-    console.log(req.query.id);
+    // console.log(req.query.id);
     try {
+        //Finding if contact to be deleted is in database
         let contact = await Contact.findById(req.query.id);
-        console.log(contact)
+        // console.log(contact)
+        //checking againg with user id. Can be skipped
         if (contact.id === req.query.id) {
+            //deleting contact
             let contactDeleted = await Contact.deleteOne({_id: req.query.id});
+            //Sending success response to user
             return res.json(200, {
                 message: "Successfully Deleted Contact",
                 data: contactDeleted
@@ -35,6 +43,44 @@ module.exports.delete = async (req, res) => {
         console.log('Error while deleting contact', e);
         return res.json(500, {
             message: 'Error while deleting contact',
+            error: e
+        })
+    }
+}
+
+module.exports.update = async (req, res) => {
+    console.log(req.query);
+    try {
+//    find if user to update exist in db
+        let contact = await Contact.findById(req.query.id);
+        console.log("contact", contact)
+        if (contact) {
+            //    user found, now update it
+            //    Checking if name is requested to be updated
+            if (req.query.name) {
+                contact.name = req.query.name;
+            }
+            //Checking if email is requested to be updated
+            if (req.query.email) {
+                contact.email = req.query.email;
+            }
+            //    Checking if mobile is requested to be updated
+            if (req.query.mobile) {
+                contact.mobile = req.query.mobile;
+            }
+            //    saving contact after updating
+            await contact.save();
+            return res.json(200, {
+                message: "Contact Updated Successfully",
+                data: contact
+            })
+        } else {
+            return res.json(404, {
+                message: "No User Found"
+            })
+        }
+    } catch (e) {
+        return res.json(500, {
             error: e
         })
     }
--- contact_list_backend/routes/api/v1/contact.js
+++ contact_list_backend/routes/api/v1/contact.js
@@ -4,4 +4,5 @@ const controllers = require("../../../controllers/api/v1/index")
 const contactController = controllers.contactController;
 router.post("/new", contactController.new);
 router.delete("/delete", contactController.delete);
+router.put("/update", contactController.update);
 module.exports = router;
